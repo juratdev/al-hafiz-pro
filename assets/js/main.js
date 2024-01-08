8835d226cbdc6921d530aa56eec2657bd650d451
juratdev <juratdev@gmail.com>
2024-01-08 13:52:59 +0500

    fix: digital input updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -537,10 +537,15 @@ $(".popup-video").magnificPopup({
 
 // input number
 
-var phoneInput = document.getElementById('phone');
+let phoneInput = document.querySelector('.phone');
+let phoneTwoInput = document.querySelector('.phone-two');
 
 phoneInput.addEventListener('input', function (e) {
-		var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
+		let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
+		e.target.value = (x[1] ? ' (' + x[1] : '') + (x[2] ? ') ' + x[2] : '') + (x[3] ? ' ' + x[3] : '') + (x[4] ? ' ' + x[4] : '');
+});
+phoneTwoInput.addEventListener('input', function (e) {
+		let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
 		e.target.value = (x[1] ? ' (' + x[1] : '') + (x[2] ? ') ' + x[2] : '') + (x[3] ? ' ' + x[3] : '') + (x[4] ? ' ' + x[4] : '');
 });
 
